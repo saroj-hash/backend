3f875672e7b5acd89889530fa557eb8f7ee98022
BATMAN <tadim777@gmail.com>
2020-04-27 01:29:46 +0530

    27-04-2020WJWS

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-const express = require('express');
-const bodyParser = require("body-parser");
-const router = require('./routes/routing');
+const express = require('express')
+const bodyParser = require("body-parser")
+const router = require('./routes/routing')
 const myErrorLogger = require('./utilities/errorlogger')
 const myRequestLogger = require('./utilities/requestlogger')
+const helmet = require("helmet")
+const cors = require('cors')
+const app = express()
 
-const cors = require('cors');
-const app = express();
+app.use(helmet())
+app.use(cors())
+app.use(bodyParser.json())
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(myRequestLogger)
+app.use('/', router)
+app.use(myErrorLogger)
 
-app.use(myRequestLogger);
-app.use('/', router);
-app.use(myErrorLogger);
-
-app.listen(3001);
-console.log("Server listening in port 3001");
+app.listen(3001)
+console.log("Server listening in port 3001")
 
 module.exports = app
+
+
+
+ 
